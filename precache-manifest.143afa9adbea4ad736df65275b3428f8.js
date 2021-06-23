63e68b6493032207460b04c8b3c8d4c6070d55f7
maryam008@hotmail.se <>
2021-06-24 00:13:40 +0200

    finished project

---
+++ precache-manifest.143afa9adbea4ad736df65275b3428f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be59d82daad0ad3b29c",
+    "url": "/css/app.cdf89a53.css"
+  },
+  {
+    "revision": "3330103855268423a240",
+    "url": "/css/chunk-vendors.25ab3afa.css"
+  },
+  {
+    "revision": "b4be1e72886349d0e842215fb10d480b",
+    "url": "/img/homeimage.b4be1e72.jpg"
+  },
+  {
+    "revision": "533ba61db98d87c44bc0d2546a6f18bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be59d82daad0ad3b29c",
+    "url": "/js/app.d6e60bea.js"
+  },
+  {
+    "revision": "3330103855268423a240",
+    "url": "/js/chunk-vendors.8b686954.js"
+  },
+  {
+    "revision": "667ea251edaaa1c8397d50910dd996bf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
